8d6b3dead560ff3d596a338561a4f8f813a73fc5
calanfive <camille.lelan@gmail.com>
2024-01-10 11:54:44 +0100

    nav App.js

--- App.js
+++ App.js
@@ -1,73 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Button from './router/Button';
+import Home from './router/Home';
+import Fil from './router/Fil';
 
-export default function App() {
-  // const [toto, setToto] = useState('toto')
+const Stack = createNativeStackNavigator();
 
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.texto}>Appuyer puis relâcher aussitôt si :</Text>
-      <View style={styles.box}>
-        <Text>Nb de piles &gt; 1 + "Exploser"</Text>
-      </View>
-      <View style={styles.box}>
-        <Text>Nb de piles &gt; + "FRK"</Text>
-      </View> 
-      <View style={styles.box}>
-        <View style={[styles.square, {backgroundColor: 'red'}]}></View>
-      </View>
-
-      <View style={styles.box}>
-        <Text style={styles.texto}>Sinon maintenir :</Text>
-      </View>
-      <View style={styles.box}>
-        <View style={[styles.rectangle, {backgroundColor: 'yellow'}]}></View>
-        <Text> = 5</Text>
-      </View>
-      <View style={styles.box}>
-        <View style={[styles.rectangle, {backgroundColor: 'blue'}]}></View>
-        <Text> = 4</Text>
-      </View>
-      <View style={styles.box}>
-        <Text>autres couleurs = 1</Text>
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Button" component={Button} />
+        <Stack.Screen name="Fil" component={Fil} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
     flex: 1
   },
-  
-  box: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  rectangle: {
-    width: 15,
-    height: 30,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: "black"
-  },
-  square: {
-    width: 30,
-    height: 30,
-    borderWidth: 1,
-    borderRadius: 50,
-    borderStyle: 'solid',
-    borderColor: "black",
-  }, 
-  texto: {
-    marginTop: 60,
-    fontWeight: "bold",
+
+  navigButton: {
+    backgroundColor: '',
   }
-  
+
 });
---
+++ routes/bouton.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function Bouton() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texto}>Appuyer puis relâcher aussitôt si :</Text>
+      <View style={styles.box}>
+        <Text>Nb de piles &gt; 1 + "Exploser"</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>Nb de piles &gt; + "FRK"</Text>
+      </View> 
+      <View style={styles.box}>
+        <View style={[styles.square, {backgroundColor: 'red'}]}></View>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.texto}>Sinon maintenir :</Text>
+      </View>
+      <View style={styles.box}>
+        <View style={[styles.rectangle, {backgroundColor: 'yellow'}]}></View>
+        <Text> = 5</Text>
+      </View>
+      <View style={styles.box}>
+        <View style={[styles.rectangle, {backgroundColor: 'blue'}]}></View>
+        <Text> = 4</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>autres couleurs = 1</Text>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flex: 1
+  },
+  
+  box: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  rectangle: {
+    width: 15,
+    height: 30,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: "black"
+  },
+  square: {
+    width: 30,
+    height: 30,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderStyle: 'solid',
+    borderColor: "black",
+  }, 
+  texto: {
+    marginTop: 60,
+    fontWeight: "bold",
+  }
+  
+});
