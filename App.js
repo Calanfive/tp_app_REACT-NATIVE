0c04ca78c48cd6dba267e706e36bb891ee24af38
calanfive <camille.lelan@gmail.com>
2024-01-10 10:57:29 +0100

    tuto 1

--- App.js
+++ App.js
@@ -1,65 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   // const [toto, setToto] = useState('toto')
 
   return (
-    <View style={styles.container}>
-      <Text>Appuyer</Text>
-      <View>
-        <View style={styles.boxjaune}>
-        </View>
-        <Text>= 5</Text>
-        <View style={styles.boxbleue}>
-        </View>
-        <Text>= 4</Text>
-        <View style={styles.boxblanc}>
-        </View>
-        <Text>= 1</Text>
-        <View style={styles.other}>
-        </View>
-        <Text>= 1</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texto}>Appuyer puis relâcher aussitôt si :</Text>
+      <View style={styles.box}>
+        <Text>Nb de piles &gt; 1 + "Exploser"</Text>
       </View>
-      <View style={styles.rela}></View>
-        <Text>Relâcher</Text>
+      <View style={styles.box}>
+        <Text>Nb de piles &gt; + "FRK"</Text>
+      </View> 
+      <View style={styles.box}>
+        <View style={[styles.square, {backgroundColor: 'red'}]}></View>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.texto}>Sinon maintenir :</Text>
+      </View>
+      <View style={styles.box}>
+        <View style={[styles.rectangle, {backgroundColor: 'yellow'}]}></View>
+        <Text> = 5</Text>
+      </View>
+      <View style={styles.box}>
+        <View style={[styles.rectangle, {backgroundColor: 'blue'}]}></View>
+        <Text> = 4</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>autres couleurs = 1</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flex: 1
   },
   
-  boxjaune: {
-    width: 30,
+  box: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  rectangle: {
+    width: 15,
     height: 30,
     borderWidth: 1,
     borderStyle: 'solid',
-    borderColor: "black",
-    backgroundColor: "yellow"
+    borderColor: "black"
   },
-  boxbleue: {
+  square: {
     width: 30,
     height: 30,
     borderWidth: 1,
+    borderRadius: 50,
     borderStyle: 'solid',
     borderColor: "black",
-    backgroundColor: "blue"
-  },
-  boxblanc: {
-    width: 30,
-    height: 30,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: "black",
-    backgroundColor: "white"
-  },
-
+  }, 
+  texto: {
+    marginTop: 60,
+    fontWeight: "bold",
+  }
+  
 });
