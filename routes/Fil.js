41b06794e5d4f4b8e1c799ef75edda1e62bbd7ec
calanfive <camille.lelan@gmail.com>
2024-01-11 11:13:09 +0100

    map colors

--- routes/Fil.js
+++ routes/Fil.js
@@ -1,17 +1,63 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { useState, useMemo } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const items = [
+    'white',
+    'red',
+    'green',
+    'black',
+    'blue',
+    'yellow'
+];
 
 export default function Fil() {
-return (
-    <SafeAreaView style={styles.container}>
-        <View>
-            <Text></Text>
-        </View>
+    const [debug, setDebug] = useState('rien')
+    const [colors, setColors] = useState(['white', 'white', 'white'])
 
-    </SafeAreaView>
-);
-}
+    const res = useMemo(() => {
+        if(colors.length === 3){
+            return 'couper le 3ème fil'
+        }
+        if(colors[0] === 'red'){
+            return 'titi'
+        }
+        return 'toto'
+    }, [colors])
+
+    const handleChangeFilColor = (indexNewColor, newColor) => {
+        const newColors = [...colors]
+        newColors[indexNewColor] = newColor
+        setColors(newColors)
+        setDebug('handleChangeColor: ' + indexNewColor + ', ' + newColor)
+    }
+
+    return (
+    //   <DropDownPicker
+    //     open={open}
+    //     value={value}
+    //     items={items}
+    //     setOpen={setOpen}
+    //     setValue={setValue}
+    //   />
+        <View>
+            {colors.map((color, k) => 
+            <View key={k}>
+                <View style={{ width: 650, height: 20, backgroundColor: color}}></View>
+                {items.map((color, keyColor) => 
+                    <TouchableOpacity 
+                        key={keyColor} 
+                        style={{ width: 50, height: 50, backgroundColor: color}} 
+                        onPress={() => handleChangeFilColor(k, color)}
+                    />
+                )}
+            </View>)}
+            <Text>{res}</Text>
+        </View>
+    );
+  }
 
 const styles = StyleSheet.create({
     button: {
